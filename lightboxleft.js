d169d0237b867658348259a90de41ec7c2518590
Maxim <tsypilov_m@skbkontur.ru>
2017-09-28 15:03:45 +0500

    new style

--- lightboxleft.js
+++ lightboxleft.js
@@ -1,15 +1,15 @@
 $(function(){
     $('.fs2_2').mouseenter(function(){ // Навели на ссылку?
-      $( ".real-hint1" ).show(); // Показываем блок
+      $( ".real-hint" ).show(); // Показываем блок
     });
 
-    $('.real-hint1').mouseleave(function(e){
-    $('.real-hint1').hide();
+    $('.real-hint').mouseleave(function(e){
+    $('.real-hint').hide();
 });
    
     $(document).click(function(e){ // Функция скрывает элемент если произошёл клик в не поля #div-test
-            if ($(e.target).closest('.real-hint1').length) return;  // Не знаю что тут происходит
-            $('.real-hint1').hide(); // Скрываем блок
+            if ($(e.target).closest('.real-hint').length) return;  // Не знаю что тут происходит
+            $('.real-hint').hide(); // Скрываем блок
             e.stopPropagation(); // Не знаю что тут происходит
         });
     
